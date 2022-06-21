7d9cad676bc0e59651533fdc0a2ee91473491114
mochammad faisal <bullenxz12@gmail.com>
2022-06-21 20:49:35 +0700

    chore: add category models

---
+++ migrations/20220621134736-create-product-category.js
@@ -0,0 +1,29 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ProductCategories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        type: Sequelize.STRING
+      },
+      productId: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ProductCategories');
+  }
+};
